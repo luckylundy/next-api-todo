bccc234a3332d4239908ef1fe619bd707ba88d8f
luckylundy <kamon3027@gmail.com>
2022-05-24 16:59:33 +0900

    サインイン不具合修正8

--- pages/index.js
+++ pages/index.js
@@ -66,9 +66,11 @@ const Home = (props) => {
       {props.todos.map((todo) => (
         <Link href={`/todo/${todo.id}`} key={todo.id}>
           <a>
-            <h3>{todo.title}</h3>
-            <h4>{todo.status}</h4>
-            <p>{todo.content}</p>
+            <div>
+              <h3>{todo.title}</h3>
+              <h4>{todo.status}</h4>
+              <h5>{todo.content}</h5>
+            </div>
           </a>
         </Link>
       ))}
