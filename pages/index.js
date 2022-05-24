f5cbe362a2facd9cece978162185ba32a594b3f8
luckylundy <kamon3027@gmail.com>
2022-05-24 23:34:03 +0900

    todo作成修正7

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/router";
 // import { useSession, signIn, signOut } from "next-auth/react";
+import { useRecoilValue } from "recoil";
+import { todosState } from "../components/atoms";
 
 const Home = (props) => {
   const [searchTitle, setSearchTitle] = useState("");
@@ -84,7 +86,8 @@ const Home = (props) => {
   );
 };
 
-export const getServerSideProps = async (context) => {
+export const getServerSideProps = async () => {
+  const [todos, setTodos] = useRecoilState(todosState);
   const response = await fetch(
     "https://next-api-todo.vercel.app/api/todo/read"
   );
@@ -93,9 +96,9 @@ export const getServerSideProps = async (context) => {
   //定数ではなく変数でtodosの値を可変にする
   // let todos = await response.json();
   // もしtodosが配列でなければ、todosに空の配列を代入する
-  // if (!Array.isArray(todos)) {
-  //   todos = [];
-  // }
+  if (!Array.isArray(todos)) {
+    setTodos([response.json()]);
+  }
 
   //fetchに失敗した場合、エラーページを表示する
   if (!todos) {
