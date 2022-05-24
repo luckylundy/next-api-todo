e075d883b0a5911f5df6333cef5b14934c0f4bd7
luckylundy <kamon3027@gmail.com>
2022-05-24 14:09:27 +0900

    サインイン不具合修正2

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,12 +11,23 @@ export default NextAuth({
     }),
   ],
   adapter: PrismaAdapter(prisma),
-  // callbacks: {
-  //   async session({ session, user }) {
-  //     //データベースのユーザーのidをsession.user.idにも代入する
-  //     session.user.id = user.id;
-  //     return Promise.resolve(session);
-  //   },
-  // },
+  callbacks: {
+    async session({ session, user }) {
+      //データベースのユーザーのidをsession.user.idにも代入する
+      session.user.id = user.id;
+      return Promise.resolve(session);
+    },
+  },
+  logger: {
+    error(code, metadata) {
+      log.error(code, metadata);
+    },
+    warn(code) {
+      log.warn(code);
+    },
+    debug(code, metadata) {
+      log.debug(code, metadata);
+    },
+  },
   secret: process.env.SECRET,
 });
